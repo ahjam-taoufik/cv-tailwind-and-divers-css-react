7dde21ac2ed6ae9b42a9837ee72e46bc876928ff
taoufik ahjam <taoufikahjam@gmail.com>
2022-05-23 14:25:18 +0200

    add Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from './pages/Contact';
 import Shop from "./pages/Shop";
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/c" element={<Contact />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div
+      className="fixed bottom-5
+    bg-slate-300 w-screen"
+    >
+      <div
+        className="flex justify-center
+      "
+      >
+        NavBar
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
